8ccfe10435ecf8b82f90f7c96778b2f6d17150c3
wangy325 <wangy325@qq.com>
2020-05-28 18:47:48 +0800

    移除netty依赖

--- consumer/src/test/java/wy/dubbo/consumer/TestDemo.java
+++ consumer/src/test/java/wy/dubbo/consumer/TestDemo.java
@@ -25,6 +25,6 @@ public class TestDemo {
     @Test
     public void testDemo() throws IOException {
         consumer.helloDubbo("wangy325");
-        System.in.read();
+//        System.in.read();
     }
 }
